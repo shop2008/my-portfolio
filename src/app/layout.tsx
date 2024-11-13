618d6899a1da8cb24a8675095243770299e294d4
Gigi <gigiwangjob@gmail.com>
2024-11-13 13:19:34 -0600

    update clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,15 +83,20 @@ export default function RootLayout({
             gtag('config', 'G-8LZ9MEJ5LV');
           `}
         </Script>
-        <Script id="clarity" strategy="afterInteractive">
-          {`
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "otuifs0ewo");
-        `}
-        </Script>
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  if(c[a])return;
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "otuifs0ewo");
+            `,
+          }}
+        />
       </head>
       <body className="font-mono">
         {children}
